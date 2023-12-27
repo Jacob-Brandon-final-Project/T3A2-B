060a6f54756d4edbe729331e78632d49044bcd58
Brandon Powell <14297@coderacademy.edu.au>
2023-12-27 21:06:05 +1000

    feat: shop filter  by started

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const ShopPage = () => {
   return (
-    <div>ShopPage</div>
+    <div className="flex min-h-screen">
+      <aside className="w-64 p-4 bg-blue-100">
+        <h2 className="text-xl font-bold mb-4">Filter By:</h2>
+        <ul>
+          <li>ALL</li>
+          <li>NBA</li>
+          <li>Magic The Gathering</li>
+          <li>Dragon Ball Z</li>
+        </ul>
+      </aside>
+    </div>
   )
 }
 
