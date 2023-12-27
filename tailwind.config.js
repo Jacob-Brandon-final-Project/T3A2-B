dd959c44186133f711963e3a4437292bc293d5aa
JacobLee42 <14303@coderacademy.edu.au>
2023-12-28 09:27:22 +1000

    uploaded and config flowbite-react

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx,cjs,mjs,html}",
-    
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+};
 
