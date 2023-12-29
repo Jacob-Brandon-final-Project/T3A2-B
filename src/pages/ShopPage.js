a9edc62b4c9098e57facc116dc45bd2629ae269e
Brandon Powell <14297@coderacademy.edu.au>
2023-12-29 11:34:12 +1000

    feat: added a drop down box for NBA

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ShopPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex min-h-screen">
-      <aside className="w-64 p-4 bg-blue-100">
+    <div className="flex min-h-screen bg-blue-100 text-white">
+      <aside className="w-64 p-4 bg-black">
         <h2 className="text-xl font-bold mb-4">Filter By:</h2>
         <ul>
           <li>ALL</li>
-          <li>NBA</li>
+          <li>
+            <div onClick={() => setIsOpen(!isOpen)}>NBA {isOpen ? '▲' : '▼'} </div>
+            {isOpen && (
+              <ul>
+                <li>Boxes</li>
+                <li>Singles</li>
+                <li>Packs</li>
+              </ul>
+            )}
+          </li>
           <li>Magic The Gathering</li>
           <li>Dragon Ball Z</li>
         </ul>
