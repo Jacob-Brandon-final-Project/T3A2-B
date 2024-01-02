2ba347f67ffe1dda19fca88114a8f37304ef3ab8
Brandon Powell <14297@coderacademy.edu.au>
2024-01-03 09:39:16 +1000

    getting shop page done and link backend

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,30 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 
 const ShopPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async (category) => {
+    try {
+      const response = await axios.get(`http://localhost:5000/AllItems?category=${category}`);
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Error fetching products', error)
+    }
+  };
+
+  const handleCategoryClick = (category) => {
+    fetchProducts(category);
+  }; 
 
   return (
     <div className="flex min-h-screen bg-blue-100 text-white">
       <aside className="w-64 p-4 bg-black">
         <h2 className="text-xl font-bold mb-4">Filter By:</h2>
         <ul>
-          <li>ALL</li>
-          <li>
-            <div onClick={() => setIsOpen(!isOpen)}>NBA {isOpen ? '▲' : '▼'} </div>
-            {isOpen && (
-              <ul>
-                <li>Boxes</li>
-                <li>Singles</li>
-                <li>Packs</li>
-              </ul>
-            )}
-          </li>
-          <li>Magic The Gathering</li>
-          <li>Dragon Ball Z</li>
+          <li onClick={() => handleCategoryClick('All')}>ALL</li>
+          <li onClick={() => handleCategoryClick('NBA')}>NBA</li>
+          <li onClick={() => handleCategoryClick('Magic')}>Magic The Gathering</li>
+          <li onClick={() => handleCategoryClick('DBZ')}>Dragon Ball Z</li>
         </ul>
       </aside>
+      
     </div>
-  )
-}
+  );
+};
 
 export default ShopPage
